ee0c215bc9339ae97637c42aed517bb251f0f0b7
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2023-09-25 22:09:36 +0530

    update: google auth response is a json

--- src/main/java/com/fixmia/rag/controllers/GoogleAuth.java
+++ src/main/java/com/fixmia/rag/controllers/GoogleAuth.java
@@ -1,6 +1,12 @@
 package com.fixmia.rag.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fixmia.rag.dtos.UserDTO;
 import com.fixmia.rag.entities.User;
+import com.fixmia.rag.entities.UserType;
+import com.fixmia.rag.util.JwtUtil;
 import com.fixmia.rag.util.ReturnMessage;
 import com.fixmia.rag.util.hibernate.AddRow;
 import com.fixmia.rag.util.hibernate.RowChecker;
@@ -11,9 +17,10 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.ws.rs.FormParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -21,11 +28,21 @@ import java.util.Collections;
 
 @Path("/")
 public class GoogleAuth {
+
+    @Inject
+    private JwtUtil jwtUtil;
     @POST
     @Path("googleoauthlogin")
-    public String postLogin(@FormParam("credential") String credential)throws GeneralSecurityException, IOException {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postLogin(@FormParam("credential") String credential)throws GeneralSecurityException, IOException {
+        System.out.println("came here");
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        ObjectNode objectNode = mapper.createObjectNode();
+
         GoogleIdToken idToken = verifyGoogleIdToken(credential);
         if (idToken != null) {
+            System.out.println("user is here and in google ");
             GoogleIdToken.Payload payload = idToken.getPayload();
 
             String userId = payload.getSubject();
@@ -33,17 +50,45 @@ public class GoogleAuth {
             // Get profile information from payload
             String email = payload.getEmail();
             // boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-            String name = (String) payload.get("name");
             boolean isUserExists = RowChecker.rowExists("User","email",email);
             if(isUserExists){
-                return ReturnMessage.successMessage("User is there");
+
+
+
+                UserDTO userDTO = new UserDTO();
+                userDTO.setEmail(email);
+                String token = jwtUtil.generateAccessToken(userDTO);
+                String rfToken = jwtUtil.generateRefreshToken(userDTO);
+                Long expiresIn = jwtUtil.getExpirationTimeInSeconds(token);
+
+                ObjectNode userDetails = mapper.createObjectNode();
+                userDetails.put("email", email);
+                objectNode.put("status", "success");
+                objectNode.put("message", "login success");
+                objectNode.put("access_token", token);
+                objectNode.put("refresh_token", rfToken);
+                objectNode.put("expires_in", expiresIn);
+                objectNode.put("token_type", "bearer");
+
+
+                objectNode.put("user", userDetails);
+                arrayNode.add(objectNode);
+
+                return Response.ok().entity(arrayNode).build();
             }else{
-                return ReturnMessage.nonException("User couldn't be found");
+                objectNode.put("status", "failed");
+                objectNode.put("message", "Not a google user");
+                arrayNode.add(objectNode);
+                System.out.println("not a google user");
+                return Response.status(Response.Status.NOT_ACCEPTABLE).entity(arrayNode).build();
             }
         } else {
 
-            System.out.println(ReturnMessage.nonException("Invalid ID token."));
-            return ReturnMessage.nonException("Invalid ID token.");
+            objectNode.put("status", "failed");
+            objectNode.put("message", "Google Couldn't verify you");
+            arrayNode.add(objectNode);
+            System.out.println("google couldn't verify you ");
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(arrayNode).build();
         }
     }
     @POST
@@ -64,9 +109,13 @@ public class GoogleAuth {
             if(isUserExists){
                 return ReturnMessage.nonException("User is a registered user");
             }else{
+                UserType userType = new UserType();
+                userType.setId(1);
+
                 User user = new User();
                 user.setEmail(email);
                 user.setUsername(name);
+                user.setUserType(userType);
                 boolean status = AddRow.addRow(user);
                 if(status)
                     return ReturnMessage.successMessage("User added successfully");
